e2ed4f808bed9ef9e2bc02e9b1e4770be6b81bd8
ElenaPupysheva <nyankem@gmail.com>
2025-10-08 17:04:53 +0300

    Настройка базовых действий БД в CardDao

--- app/src/main/java/com/example/myapplication/data/db/CardDao.kt
+++ app/src/main/java/com/example/myapplication/data/db/CardDao.kt
@@ -1,8 +1,22 @@
 package com.example.myapplication.data.db
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface CardDao {
-    //TODO
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCard(card: CardEntity)
+
+    @Delete
+    suspend fun deleteCard(card: CardEntity)
+
+    @Query("SELECT * FROM cards")
+    suspend fun getAllCards(): List<CardEntity>
+
+    @Query("SELECT * FROM cards WHERE id = :id")
+    suspend fun getCardById(id: Long): CardEntity?
 }
