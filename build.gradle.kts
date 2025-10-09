355d3ba7f8dec82e7859c82153ee952b02f0e701
leonid-l-a <leo.lapaeff@gmail.com>
2025-10-09 11:28:54 +0200

    GH-1 detekt setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,53 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.detekt)
+}
+
+tasks.withType<Detekt>().configureEach {
+    parallel = true
+    autoCorrect = false
+    buildUponDefaultConfig = true
+    disableDefaultRuleSets = false
+
+    jvmTarget = "17"
+
+    setSource(files("app/src"))
+    include("**/*.kt", "**/*.kts")
+    exclude("**/resources/**", "**/build/**")
+
+    config.setFrom(files(project.rootDir.resolve("conf/custom-detekt.yml")))
+
+    reports {
+        xml.required.set(false)
+        html.required.set(true)
+        txt.required.set(true)
+    }
+}
+
+tasks.register<Detekt>("detektAll") {
+    description = "Runs Detekt on the app module"
+    group = "verification"
+    parallel = true
+    buildUponDefaultConfig = true
+    jvmTarget = "17"
+
+    setSource(files("app/src"))
+    include("**/*.kt", "**/*.kts")
+    exclude("**/resources/**", "**/build/**")
+
+    config.setFrom(files(project.rootDir.resolve("conf/custom-detekt.yml")))
+
+    reports {
+        html.required.set(true)
+        txt.required.set(true)
+    }
+}
+
+dependencies {
+    detektPlugins(libs.staticAnalysis.detektFormatting)
+    detektPlugins(libs.staticAnalysis.detektLibraries)
 }
