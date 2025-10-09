7418b481701048f619113cf324c46b6329fc7fc4
Zdertis420 <AnRobo07@yandex,ru>
2025-10-09 18:46:52 +0300

    Подключён ktor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,8 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
