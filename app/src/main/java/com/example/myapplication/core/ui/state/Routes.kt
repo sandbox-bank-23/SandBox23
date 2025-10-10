495ff4e12459fe6478bf10d12aa6d55f48094195
Vlad <vladismann@yandex.ru>
2025-10-10 20:20:22 +0300

    перенос вспомогательных классов

---
+++ app/src/main/java/com/example/myapplication/core/ui/state/Routes.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.core.ui.state
+
+enum class Routes(val route: String) {
+    CARDS("cards"),
+    FINANCE("finance"),
+    TRANSFERS("transactions"),
+    HISTORY("history"),
+    PROFILE("profile")
+}
