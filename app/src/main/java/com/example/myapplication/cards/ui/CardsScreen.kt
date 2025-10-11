bee74dc8c4d40fa6aa03dc8f20513e72f155b11a
owenear <owenear@gmail.com>
2025-10-11 20:03:43 +0300

    реализованы функции отрисовки карты с основными данными

--- app/src/main/java/com/example/myapplication/cards/ui/CardsScreen.kt
+++ app/src/main/java/com/example/myapplication/cards/ui/CardsScreen.kt
@@ -11,25 +11,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.R
+import java.text.DecimalFormat
 
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 fun CardsScreen(
     //viewModel: CardsViewModel = koinViewModel<CardsViewModel>()
@@ -38,7 +41,7 @@ fun CardsScreen(
         modifier = Modifier
             .fillMaxSize(1f)
     ) {
-        CardItem2()
+        CardItem()
         ExtendedFloatingActionButton(
             modifier = Modifier.fillMaxWidth().padding(16.dp, 8.dp),
             onClick = {  },
@@ -58,100 +61,140 @@ fun CardsScreen(
 
 @Preview
 @Composable
-fun CardItem() {
+fun CardItem(
+    cardHolderName: String = "SandboxBank",
+    cardName: String? = null,
+    cardNumber: String? = null
+) {
     Box(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
             .height(205.dp)
+            .width(328.dp)
+            .padding(0.dp)
             .background(
                 color = Color(0XFFE0D9FF),
                 shape = RoundedCornerShape(16.dp)
             )
+            .clip(RoundedCornerShape(16.dp))
     ) {
         Box(
             modifier = Modifier
-                .size(200.dp)
-                .offset(x = 224.dp, y = -60.dp)
-                .background(
-                    color = Color(0xFFFFD7B6),
-                    shape = CircleShape
-                )
-        )
-        Box(
-            modifier = Modifier
-                .size(350.dp)
-                .offset(x = -130.dp, y = 25.dp)
+                .requiredSize(350.dp)
+                .offset(x = (-120).dp, y = 25.dp)
                 .background(
                     color = Color(0xFFD2F1E4),
                     shape = CircleShape
                 )
         )
-        Column(
-            modifier = Modifier.fillMaxWidth().padding(0.dp),
-            verticalArrangement = Arrangement.SpaceAround
-        ) {
-            Text(
-                modifier = Modifier.fillMaxWidth().padding(22.dp, 22.dp),
-                color = Color(0XFF222222),
-                textAlign = TextAlign.Start,
-                text = "SandboxBank",
-                maxLines = 1,
-                style = MaterialTheme.typography.headlineMedium
-            )
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                Text(
-                    modifier = Modifier.fillMaxWidth().padding(22.dp, 22.dp),
-                    color = Color(0XFF222222),
-                    textAlign = TextAlign.Start,
-                    text = "1 000 000 \u20BD",
-                    maxLines = 1,
-                    style = MaterialTheme.typography.headlineMedium
+        Box(
+            modifier = Modifier
+                .requiredSize(190.dp)
+                .offset(x = 225.dp, y = (-60).dp)
+                .padding(0.dp)
+                .background(
+                    color = Color(0xFFFFD7B6),
+                    shape = CircleShape
                 )
+        )
+        Column(
+            modifier = Modifier.fillMaxSize().padding(vertical = 24.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            CardHolderName(cardHolderName)
+            Column {
+                CardData(cardName, cardNumber)
+                CardBalance(1000000f)
             }
         }
     }
 }
 
-@Preview
+
 @Composable
-fun CardItem2() {
-    Box(
-        modifier = Modifier.fillMaxWidth()
-            .height(265.dp)
-            .width(367.dp)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.card), // ваша картинка
-            contentDescription = "Card background",
-            modifier = Modifier.fillMaxSize().padding(),
-            contentScale = ContentScale.Crop // Обрезает картинку под размер
+private fun CardHolderName(name: String) {
+    Text(
+        modifier = Modifier.padding(horizontal = 24.dp),
+        color = Color(0XFF222222),
+        textAlign = TextAlign.Start,
+        text = name,
+        maxLines = 1,
+        style = MaterialTheme.typography.headlineMedium
+    )
+}
+
+
+@Composable
+private fun CardData(cardName: String?, cardNumber: String?) {
+    Column() {
+        if (!cardName.isNullOrEmpty())
+        Text(
+            modifier = Modifier.padding(horizontal = 24.dp, vertical = 4.dp).height(16.dp),
+            color = Color(0XFF222222),
+            textAlign = TextAlign.Start,
+            text = cardName,
+            maxLines = 1,
+            style = MaterialTheme.typography.bodySmall
         )
-        Column(
-            modifier = Modifier.fillMaxWidth().padding(16.dp),
-            verticalArrangement = Arrangement.SpaceAround
-        ) {
-            Text(
-                modifier = Modifier.fillMaxWidth().padding(22.dp, 22.dp),
-                color = Color(0XFF222222),
-                textAlign = TextAlign.Start,
-                text = "SandboxBank",
-                maxLines = 1,
-                style = MaterialTheme.typography.headlineMedium
+        if (!cardNumber.isNullOrEmpty())
+        Text(
+            modifier = Modifier.padding(horizontal = 18.dp, vertical = 4.dp).height(24.dp),
+            color = Color(0XFF222222),
+            textAlign = TextAlign.Start,
+            text = cardNumber,
+            maxLines = 1,
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Composable
+private fun CardBalance(balance: Float) {
+    val balanceFormat = DecimalFormat("#,##0 \u20BD").format(balance)
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 24.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            modifier = Modifier.height(28.dp),
+            color = Color(0XFF222222),
+            textAlign = TextAlign.Start,
+            text = balanceFormat,
+            maxLines = 1,
+            style = MaterialTheme.typography.titleLarge
+        )
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.Bottom,
+            modifier = Modifier.height(20.dp),
+        ){
+            Icon(
+                painterResource(R.drawable.ic_card_mir_logo1),
+                "Extended floating action button."
             )
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceAround
+            Column(
+                modifier = Modifier.padding(start = 1.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
-                    modifier = Modifier.fillMaxWidth().padding(22.dp, 22.dp),
-                    color = Color(0XFF222222),
-                    textAlign = TextAlign.Start,
-                    text = "1 000 000 \u20BD",
-                    maxLines = 1,
-                    style = MaterialTheme.typography.headlineMedium
-                )
+                Row(
+                    modifier = Modifier.height(9.dp),
+                    verticalAlignment = Alignment.Top
+                ) {
+                    Icon(
+                        painterResource(R.drawable.ic_card_mir_logo2),
+                        "Extended floating action button."
+                    )
+                }
+                Row(
+                    modifier = Modifier.height(11.dp).padding(0.dp),
+                    verticalAlignment = Alignment.Top
+                ) {
+                    Icon(
+                        painterResource(R.drawable.ic_card_mir_logo3),
+                        "Extended floating action button."
+                    )
+                }
             }
         }
     }
