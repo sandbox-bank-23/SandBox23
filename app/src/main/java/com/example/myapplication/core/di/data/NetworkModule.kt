d6c06e77f936107c3a648dcd02ba6582bbd4d0fc
Zdertis420 <AnRobo07@yandex,ru>
2025-10-13 18:44:28 +0300

    Забыл DI

--- app/src/main/java/com/example/myapplication/core/di/data/NetworkModule.kt
+++ app/src/main/java/com/example/myapplication/core/di/data/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.core.di.data
 
+import com.example.myapplication.core.data.network.NetworkClient
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import org.koin.dsl.module
@@ -9,4 +10,6 @@ val networkModule = module {
     single<HttpClient> {
         HttpClient(Android)
     }
+
+    single<NetworkClient> { NetworkClient(get<HttpClient>()) }
 }
