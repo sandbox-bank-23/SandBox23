5e76d903bfe5b707b0c3c64aa33bb5e273e1efbf
Zdertis420 <AnRobo07@yandex,ru>
2025-10-13 12:46:39 +0300

    фиксы по детекту 5

--- app/src/main/java/com/example/myapplication/auth/data/mock/AuthMock.kt
+++ app/src/main/java/com/example/myapplication/auth/data/mock/AuthMock.kt
@@ -1,4 +1,4 @@
-@file:Suppress("MagicNumber", "ReturnCount", "MaxLineLength")
+@file:Suppress("MagicNumber")
 
 package com.example.myapplication.auth.data.mock
 
@@ -7,91 +7,64 @@ import java.util.Base64
 import kotlin.random.Random
 
 class AuthMock {
-    fun getLogin(): Response {
-        when (Random.nextInt(100)) {
-            in 0..79 -> {
-                return Response(
-                    code = 200,
-                    description = "OK",
-                    response = """"access_token": ${formToken()}
-"refresh_token": ${formToken()}
-"user_id": ${Random.nextInt()}""""
-                )
-            }
-
-            in 80..89 -> {
-                return Response(
-                    code = 400,
-                    description = "Invalid email",
-                    response = null
-                )
-            }
-
-            in 90..99 -> {
-                return Response(
-                    code = 400,
-                    description = "Invalid password",
-                    response = null
-                )
-            }
-
-            // Данный ответ есть, просто потому что без него студия пишет, что нет return
-            else -> {
-                return Response(
-                    code = 420,
-                    description = "No",
-                    response = null
-                )
-            }
-        }
+    fun getLogin(): Response = when (Random.nextInt(100)) {
+        in 0..79 -> loginSuccess()
+        in 80..89 -> invalidEmail()
+        in 90..99 -> invalidPassword()
+        else -> responseNo()
     }
 
-    fun getRegister(): Response {
-        when (Random.nextInt(1, 100)) {
-            in 1..85 -> {
-                return Response(
-                    code = 201,
-                    description = "Created",
-                    response = """"access_token": ${formToken()}
-"refresh_token": ${formToken()}
-"user_id": ${Random.nextInt()}""""
-                )
-            }
-
-            in 86..90 -> {
-                return Response(
-                    code = 409,
-                    description = "User exists",
-                    response = null
-                )
-            }
-
-            in 91..95 -> {
-                return Response(
-                    code = 400,
-                    description = "Invalid email",
-                    response = null
-                )
-            }
-
-            in 96..100 -> {
-                return Response(
-                    code = 400,
-                    description = "Invalid password",
-                    response = null
-                )
-            }
-
-            // Данный ответ есть, просто потому что без него студия пишет, что нет return
-            else -> {
-                return Response(
-                    code = 420,
-                    description = "No",
-                    response = null
-                )
-            }
-        }
+    fun getRegister(): Response = when (Random.nextInt(1, 100)) {
+        in 1..85 -> registerSuccess()
+        in 86..90 -> userExists()
+        in 91..95 -> invalidEmail()
+        in 96..100 -> invalidPassword()
+        else -> responseNo()
     }
+
+    private fun loginSuccess(): Response = Response(
+        code = 200,
+        description = "OK",
+        response = """
+            "access_token": ${formToken()}
+            "refresh_token": ${formToken()}
+            "user_id": ${Random.nextInt()}
+            """.trimIndent()
+    )
+
+    private fun registerSuccess(): Response = Response(
+        code = 201,
+        description = "Created",
+        response = """
+            "access_token": ${formToken()}
+            "refresh_token": ${formToken()}
+            "user_id": ${Random.nextInt()}
+            """.trimIndent()
+    )
+
+    private fun invalidEmail(): Response = Response(
+        code = 400,
+        description = "Invalid email",
+        response = null
+    )
+
+    private fun invalidPassword(): Response = Response(
+        code = 400,
+        description = "Invalid password",
+        response = null
+    )
+
+    private fun userExists(): Response = Response(
+        code = 409,
+        description = "User exists",
+        response = null
+    )
+
+    private fun responseNo(): Response = Response(
+        code = 420,
+        description = "No",
+        response = null
+    )
 
     private fun formToken(): String =
         Base64.getEncoder().withoutPadding().encodeToString(Random.Default.nextBytes(32))
