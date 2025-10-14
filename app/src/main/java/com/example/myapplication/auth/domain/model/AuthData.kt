c73fd3a17548ef198527e94aeeeabb1e05df364f
n1k1ttka <nikita.kolgushkin@mail.ru>
2025-10-14 09:27:26 +0300

    data+domain цепочка для Auth (completed)

--- app/src/main/java/com/example/myapplication/auth/domain/model/AuthData.kt
+++ app/src/main/java/com/example/myapplication/auth/domain/model/AuthData.kt
@@ -3,5 +3,7 @@ package com.example.myapplication.auth.domain.model
 data class AuthData(
     val code: Int,
     val description: String,
-    val response: String?
+    val accessToken: String?,
+    val refreshToken: String?,
+    val userId: String?
 )
--- app/src/main/java/com/example/myapplication/auth/domain/state/Result.kt
+++ app/src/main/java/com/example/myapplication/auth/domain/state/Result.kt
@@ -1,6 +1,6 @@
 package com.example.myapplication.auth.domain.state
 
-sealed class Result<T> {
+sealed class Result<out T> {
     data class Success<T>(val data: T) : Result<T>()
     data class Error(val message: String?) : Result<Nothing>()
 
