ea17250ecdd7dd8e8dfada75218585dd4f64f669
ElenaPupysheva <nyankem@gmail.com>
2025-10-17 10:54:32 +0300

    Исправления по detekt

--- app/src/main/java/com/example/myapplication/debitcards/domain/api/DebitCardsRepository.kt
+++ app/src/main/java/com/example/myapplication/debitcards/domain/api/DebitCardsRepository.kt
@@ -1,7 +1,7 @@
 package com.example.myapplication.debitcards.domain.api
 
-import com.example.myapplication.core.domain.models.Card
 import com.example.myapplication.core.data.model.Result
+import com.example.myapplication.core.domain.models.Card
 
 interface DebitCardsRepository {
    suspend fun createDebitCard(
