d25ffbbac514f21007eea080ddaf8b742506ca02
ElenaPupysheva <nyankem@gmail.com>
2025-10-19 15:36:36 +0300

    LoansDepositsScreen разработка

--- app/src/main/java/com/example/myapplication/loansanddeposits/ui/screen/LoansDepositsScreen.kt
+++ app/src/main/java/com/example/myapplication/loansanddeposits/ui/screen/LoansDepositsScreen.kt
@@ -1,13 +1,68 @@
 package com.example.myapplication.loansanddeposits.ui.screen
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.myapplication.R
+import com.example.myapplication.core.ui.theme.AppTypography
+import com.example.myapplication.core.ui.theme.PaddingBase
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoansDepositsScreen(
-
+    navController: NavHostController,
+    onBackClick: () -> Unit
 ) {
-    Column {
-
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                windowInsets = androidx.compose.foundation.layout.WindowInsets(0),
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onBackground,
+                    actionIconContentColor = MaterialTheme.colorScheme.onBackground
+                ),
+                navigationIcon = {
+                    IconButton(
+                        onClick = { navController.popBackStack() },
+                        modifier = Modifier
+                            .size(48.dp)
+                            .padding(start = PaddingBase)
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.arrow_back),
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+                },
+                title = {
+                    Text(
+                        text = stringResource(R.string.loans_and_deposits), // должно соответствовать названию экрана
+                        style = AppTypography.titleLarge,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier.padding(start = PaddingBase)
+                    )
+                }
+            )
+        }
+    ) {
     }
 }
