497732f98e17405b6693091fe6026fb52c4873c8
ElenaPupysheva <nyankem@gmail.com>
2025-10-19 12:42:29 +0300

    Создание базовых фалов

---
+++ app/src/main/java/com/example/myapplication/loansanddeposits/ui/screen/LoansDepositsScreen.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.loansanddeposits.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoansDepositsScreen(
+
+) {
+    Column {
+
+    }
+}
