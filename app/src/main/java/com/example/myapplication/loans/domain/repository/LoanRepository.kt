d834e1fa2eeac0b0e57b3a2dab235ba3d2a5be5d
Valery Karev <karev@34web.ru>
2025-10-28 16:13:21 +0300

    GH-127-FIX 1. Фикс крита по отображению списка кредитов и депозитов 2. Фикс крита по созданию кредита

--- app/src/main/java/com/example/myapplication/loans/domain/repository/LoanRepository.kt
+++ app/src/main/java/com/example/myapplication/loans/domain/repository/LoanRepository.kt
@@ -2,12 +2,9 @@ package com.example.myapplication.loans.domain.repository
 
 import com.example.myapplication.loans.domain.model.Credit
 import com.example.myapplication.loans.domain.model.LoanResult
-import com.example.myapplication.loans.domain.model.Pay
 import kotlinx.coroutines.flow.Flow
-import java.math.BigDecimal
 
 interface LoanRepository {
-    suspend fun calculate(sum: BigDecimal, period: Long, percent: Int): Pay?
     suspend fun create(loan: Credit): Flow<LoanResult>
 
     suspend fun getLoan(loanId: Long): Flow<LoanResult>
