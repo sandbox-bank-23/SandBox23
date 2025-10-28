c0b5a8a1ec4e8a1dd3030bac018bf7ffde83d647
Zdertis420 <AnRobo07@yandex,ru>
2025-10-28 23:11:25 +0300

    по просьбе команды

--- app/src/main/java/com/example/myapplication/profile/data/repo/UpdatesRepositoryImpl.kt
+++ app/src/main/java/com/example/myapplication/profile/data/repo/UpdatesRepositoryImpl.kt
@@ -1,13 +1,40 @@
+@file:Suppress("MagicNumber")
+
 package com.example.myapplication.profile.data.repo
 
 import com.example.myapplication.core.data.network.Response
 import com.example.myapplication.profile.domain.api.UpdatesRepository
 import kotlinx.serialization.json.Json
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class UpdatesRepositoryImpl : UpdatesRepository {
-    override fun isLatestVersion() = Response(
-        code = 200,
-        description = "OK",
-        response = Json.encodeToString(true)
-    )
+
+    @Suppress("MaxLineLength")
+    override fun isLatestVersion() = when (Random.nextInt(1..1000)) {
+        in 1..900 -> Response(
+            code = 200,
+            description = "OK",
+            response = Json.encodeToString(true)
+        )
+
+        in 901..998 -> Response(
+            code = 200,
+            description = "OK",
+            response = Json.encodeToString(false)
+        )
+
+        in 999..1000 -> Response(
+            code = 420,
+            description = "no",
+            response = Json.encodeToString(NullPointerException())
+        )
+
+        else -> Response(
+            code = 0,
+            description = "",
+            response = Json.encodeToString("")
+        )
+
+    }
 }
